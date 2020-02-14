32e711e78a6809a1959b9f8f9f8a34fb0dc19979
Joshua <Jplittle2001@gmail.com>
2020-02-13 23:01:32 -0500

    update not working

--- src/board.rs
+++ src/board.rs
@@ -1,16 +1,26 @@
 
-enum Tile {
+pub enum Tile {
     Empty,
     Value(usize),
 }
 
-struct Board {
+pub enum Direction {
+    Up,
+    Down,
+    Left,
+    Right,
+}
+
+pub struct Board {
+    width: usize,
+    height: usize,
     data: Vec<Vec<Tile>>,
 }
 
 impl Board {
-    fn new(width: usize, height: usize) -> Board {
+    pub fn new(width: usize, height: usize) -> Board {
 
+        // initialize the data with empty tiles
         let mut data = Vec::with_capacity(height);
         for ri in 0..height {
             let mut row = Vec::with_capacity(width);
@@ -20,9 +30,98 @@ impl Board {
             data.push(row);
         }
 
-
         Board {
+            width: width,
+            height: height,
             data: data,
         }
+    }
+
+    pub fn new_square(size: usize) -> Board {
+        Board::new(size, size)
+    }
+
+    pub fn is_within(&self, x: isize, y: isize) -> bool {
+        x >= 0 && x < self.width as isize && y >= 0 && y < self.height as isize
+    }
+
+    pub fn set_tile(&mut self, x: usize, y: usize, tile: Tile) {
+        self.data[y][x] = tile;
+    }
+
+    pub fn get_tile(&self, x: usize, y: usize) -> &Tile {
+        &self.data[y][x]
+    }
+
+    pub fn make_move(&mut self, dir: Direction) -> bool {
+
+        let mut has_changed = false;
+
+        let dir_vec = match dir {
+            Direction::Up => (0, -1),
+            Direction::Down => (0, 1),
+            Direction::Left => (-1, 0),
+            Direction::Right => (1, 0),
+            _ => (0, 0)
+        };
+
+        // if the move vector is zero then nothing will occur on the game board
+        if dir_vec == (0, 0) {
+            return false;
+        }
+
+        //let mut xrange: std::iter::Iterator = 0..self.width;
+        //let mut yrange: std::iter::Iterator = 0..self.height;
+        
+        // flip the ranges to go the opposite way for the moveing/merging
+        let xrange = if dir_vec.0 < 0 {
+            0..self.width
+        } else {
+            (0..self.width).rev()
+        };
+        // if dir_vec.1 < 0 {
+        //     yrange = yrange.rev();
+        // }
+
+        // for xi in xrange {
+        //     for yi in yrange {
+
+        //     }
+        // }
+
+
+
+        // for ai in {
+
+        //     for bi in 
+
+        //     if dir_vec[0] != 0 {
+
+        //     }
+
+        // }
+
+        return has_changed;
+
+    }
+
+    pub fn print(&self) {
+
+        let mut character: &str;
+        for yi in 0..self.height {
+            for xi in 0..self.width {
+                
+                character = match self.get_tile(xi, yi) {
+                    Tile::Empty => "_",
+                    Tile::Value(v) => "A",
+                    _ => "?",
+                };
+                print!("{}", character);
+                if xi != self.width - 1 {
+                    print!(" ");
+                }
+            }
+            println!("");
+        } 
     }
 }
--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,19 @@
 
 mod board;
 
-//use crate::board;
+use crate::board::{Board, Tile, Direction};
 
 fn main() {
-    println!("Hello, world!");
+
+    let mut board: Board = Board::new_square(4);
+
+
+    
+    board.set_tile(0,0,Tile::Value(10));
+    board.set_tile(2,0,Tile::Value(10));
+    board.print();
+    println!("");
+    board.make_move(Direction::Right);
+    board.print();
+
 }
